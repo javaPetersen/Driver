89aafdf3bfbf55feff02966e5ca968d2f75730d5
Piotr Perlinski <perlinskipiotr@wp.pl>
2021-10-11 15:23:10 +0200

    tag rest controller added with getmapping and postmapping

---
+++ src/main/java/pl/driver/driver/controller/TagRestController.java
@@ -0,0 +1,59 @@
+package pl.driver.driver.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.driver.driver.entity.Advice;
+import pl.driver.driver.entity.Tag;
+import pl.driver.driver.service.AdviceService;
+import pl.driver.driver.service.TagService;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api")
+public class TagRestController {
+
+    private final TagService tagService;
+    private final AdviceService adviceService;
+
+    @Autowired
+    public TagRestController(TagService tagService, AdviceService adviceService) {
+        this.tagService = tagService;
+        this.adviceService = adviceService;
+    }
+
+    @GetMapping("/tag")
+    public ResponseEntity<List<Tag>> getAllTags() {
+        return ResponseEntity.status(HttpStatus.OK).body(tagService.findAll());
+    }
+
+    @GetMapping("/tag/{id}")
+    public ResponseEntity<Tag> getTagById(@PathVariable Long id) {
+        return ResponseEntity.of(tagService.getTagById(id));
+    }
+
+    @PostMapping("/advice/{id}/tag")
+    public ResponseEntity<Void> addTagToAdvice(@PathVariable Long id, @Valid @RequestBody Tag... tag) {
+        Optional<Advice> optionalAdvice = adviceService.get(id);
+        if (optionalAdvice.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        Set<@Valid Tag> tags = Arrays.stream(tag).collect(Collectors.toSet());
+        optionalAdvice.get().getTags().forEach(tags::remove);
+
+        tags.stream()
+                .peek(t -> t.getAdvices().add(optionalAdvice.get()))
+                .forEach(tagService::save);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
+
+
+}
