d2d68384021382fc4bc6961de3d9507d178224b8
Piotr Perlinski <perlinskipiotr@wp.pl>
2021-10-13 14:00:01 +0200

    rebuild code, cleaned controller, move some logic to service

--- src/main/java/pl/driver/driver/service/AdviceServiceImpl.java
+++ src/main/java/pl/driver/driver/service/AdviceServiceImpl.java
@@ -1,17 +1,12 @@
 package pl.driver.driver.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 import pl.driver.driver.entity.Advice;
 import pl.driver.driver.repository.AdviceRepository;
-import pl.driver.driver.repository.TagRepository;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,24 +14,20 @@ import java.util.Optional;
 public class AdviceServiceImpl implements AdviceService{
 
     private final AdviceRepository adviceRepository;
-    private final TagRepository tagRepository;
 
     @Autowired
-    public AdviceServiceImpl(AdviceRepository adviceRepository,
-                             TagRepository tagRepository) {
+    public AdviceServiceImpl(AdviceRepository adviceRepository) {
         this.adviceRepository = adviceRepository;
-        this.tagRepository = tagRepository;
     }
-
 
     @Override
     public void save(Advice advice) {
+        advice.setId(null);
         adviceRepository.save(advice);
     }
 
-
     @Override
-    public Optional<Advice> get(Long id) {
+    public Optional<Advice> getAdvice(Long id) {
         return adviceRepository.findById(id);
     }
 
@@ -46,28 +37,9 @@ public class AdviceServiceImpl implements AdviceService{
     }
 
     @Override
-    public Boolean remove(Long id) {
-        Optional<Advice> optionalAdvice = adviceRepository.findById(id);
-        if (optionalAdvice.isEmpty()) {
-           return false;
-        }
-        adviceRepository.deleteById(id);
-        return true;
-    }
-
-    @Override
-    public String storeFile(MultipartFile file, Advice advice) {
-        if (!Files.exists(Paths.get(uploadPath))) {
-            createUploadDir();
-        }
-        try {
-            Path newFilePath = Paths.get(uploadPath + file.getOriginalFilename());
-            file.transferTo(newFilePath);
-            return newFilePath.toString();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public void remove(Long id) {
+        Advice advice = checkIfAdviceExists(id);
+        adviceRepository.deleteById(advice.getId());
     }
 
     @Override
@@ -75,14 +47,21 @@ public class AdviceServiceImpl implements AdviceService{
         adviceRepository.saveAll(advices);
     }
 
-    @Value("${upload.path}")
-    private String uploadPath;
+    @Override
+    public void update(Advice editedAdvice, Long oldAdviceId) {
+        Advice oldAdvice = checkIfAdviceExists(oldAdviceId);
+        oldAdvice.setTitle(editedAdvice.getTitle());
+        oldAdvice.setContent(editedAdvice.getContent());
+        oldAdvice.setTags(editedAdvice.getTags());
+        oldAdvice.setTraining(editedAdvice.getTraining());
+        adviceRepository.save(oldAdvice);
+    }
 
-    private void createUploadDir() {
-        try {
-            Files.createDirectories(Paths.get(uploadPath));
-        } catch (IOException e) {
-            throw new RuntimeException("Could not create upload folder!");
+    private Advice checkIfAdviceExists(Long adviceId) {
+        Optional<Advice> optionalAdvice = adviceRepository.findById(adviceId);
+        if (optionalAdvice.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Advice not found");
         }
+        return optionalAdvice.get();
     }
 }
